b473099bb10e9aafa7b6bfe611139066e10c9b12
victoria makena <makenavictoria1@gmail.com>
2022-04-18 23:05:53 +0300

    remove some components and services

--- src/app/components/search-form/search-form.component.ts
+++ src/app/components/search-form/search-form.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+ 
 @Component({
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
   styleUrls: ['./search-form.component.css']
 })
 export class SearchFormComponent implements OnInit {
-  results:any[]=[];
+  user:any[]=[];
 
   constructor() { }
 
