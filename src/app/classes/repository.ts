f3eee173c907ab5856f6bd29ad88394dcc1ae68a
victoria makena <makenavictoria1@gmail.com>
2022-04-18 16:53:19 +0300

    define repositories class

--- src/app/classes/repository.ts
+++ src/app/classes/repository.ts
@@ -1,2 +1,10 @@
 export class Repository {
+    constructor(
+        public name: string,
+        public url: string,
+        public description: string,
+        public language: string,
+        public created: Date,
+        public forks:number,
+    ){}
 }
