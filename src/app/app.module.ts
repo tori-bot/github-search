a7e67915a54606cb45cbbda7bd64bc1890b3e667
victoria makena <makenavictoria1@gmail.com>
2022-04-19 04:47:29 +0300

    configure page routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { HighlightDirective } from './directives/highlight.directive';
 import { CreatedAtPipe } from './pipes/created-at.pipe';
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
